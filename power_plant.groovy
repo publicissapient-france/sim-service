f1c8af5d796886e840b67c26bed42888ff8787fe
Aurélien Maury <amaury@xebia.fr>
2014-06-12 10:40:04 +0200

    run.sh fixed

--- power_plant.groovy
+++ power_plant.groovy
@@ -7,25 +7,51 @@ import org.vertx.groovy.core.Vertx
 Vertx vx = vertx
 Container dock = container
 
+/**
+ * Configuration
+ */
 def me = [
         id: UUID.randomUUID().toString(),
-        type: 'power.plant'
+        type: 'power.plant',
+]
+
+def channels = [
+        output: [
+                status: me.type + '.status'
+        ],
+        input: [
+                consume: me.type + 'consume'
+        ]
 ]
 
 def conf = [
         maxLoad: (dock.config[me.type]?.maxLoad ?: 10).toInteger(),
-        initLoad: (dock.config[me.type]?.initLoad ?: 0).toInteger()
+        initLoad: (dock.config[me.type]?.initLoad ?: 0).toInteger(),
+        productionPeriod: (dock.config[me.type]?.productionPeriod ?: 1000).toInteger(),
+        startChannel: 'monitoring.service.start'
 ]
 
+/**
+ * Debug listeners
+ */
+vx.eventBus.registerHandler(channels.output.status) { message ->
+    println "OUTPUT ${channels.output.status} => ${message.body}"
+}
 
+vx.eventBus.registerHandler(conf.startChannel) { message ->
+    println "OUTPUT ${conf.startChannel} => ${message.body}"
+}
 
+/**
+ * Service itself
+ */
 def emit = { String busAddress, Map message ->
     vx.eventBus.publish(busAddress, me + message)
 }
 
 AtomicInteger load = new AtomicInteger(conf.initLoad)
 
-def timerID = vx.setPeriodic(1000) { timerID ->
+vx.setPeriodic(conf.productionPeriod) { timerID ->
     def newLoad = load.incrementAndGet()
 
     if (newLoad > conf.maxLoad) {
@@ -33,10 +59,15 @@ def timerID = vx.setPeriodic(1000) { timerID ->
         newLoad = conf.maxLoad
     }
 
-    emit("${me.type}.status", [load: newLoad])
+    emit(channels.output.status, [load: newLoad])
 }
 
+vx.eventBus.registerHandler(channels.input.consume) { message ->
+    if (message.body.need && message.body.replyTo) {
 
-vx.eventBus.registerHandler('power.plant.status') { message ->
-    println message.body
+    }
 }
+
+emit(conf.startChannel,  channels)
+
+
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-vertx runmod io.vertx~mod-web-server~2.0.0-final -conf ./vertx.webserver.conf
+vertx run power_plant.groovy -conf power_plant.conf
