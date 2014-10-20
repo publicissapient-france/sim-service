11a40cda011eb60e3f5b36c1bd245cbcc023fe72
RomainN <romain.niveau@gmail.com>
2014-10-20 16:18:14 +0200

    #15 Service Farm parametrable

--- services/farm/src/main/java/Farm.java
+++ services/farm/src/main/java/Farm.java
@@ -1,3 +1,4 @@
+import org.vertx.java.core.AsyncResult;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.eventbus.EventBus;
 import org.vertx.java.core.eventbus.Message;
@@ -19,35 +20,59 @@ public class Farm extends Verticle {
         EventBus eventBus = vertx.eventBus();
         JsonObject config = container.config();
 
-        String id = UUID.randomUUID().toString();
+        String id = "farm-" + UUID.randomUUID().toString();
         JsonObject adresses = config.getObject("adresses");
+        stock = config.getInteger("initialStock");
         Integer cost = config.getInteger("cost");
 
         sayHello(eventBus, adresses, id);
 
 
         Handler<Message<JsonObject>> myHandler = message -> {
-            Number quantity = message.body().getNumber("quantity");
+            container.logger().info("Received a request");
+            Integer quantity = message.body().getInteger("quantity");
             if (quantity != null) {
                 container.logger().info(("Farm get a message for stock " + quantity));
-                JsonObject reply = new JsonObject();
-                reply.putString("from", "farm " + id);
-                reply.putString("action", "response");
+                JsonObject offer = new JsonObject();
+                offer.putString("from", id);
+                offer.putString("action", "response");
                 Integer availableStock = stock - promisedStock;
-                int availableQuantity = Math.min(availableStock, quantity.intValue());
-                promisedStock += availableStock;
+                int availableQuantity = Math.min(availableStock, quantity);
+                promisedStock += availableQuantity;
                 container.logger().info(("Farm reply with " + availableQuantity));
-                reply.putNumber("quantity", availableQuantity);
-                reply.putNumber("cost", (availableQuantity * cost));
-                message.reply(reply);
+                offer.putNumber("quantity", availableQuantity);
+                offer.putNumber("cost", (availableQuantity * cost));
+                container.logger().info("stock=" + stock + " promisedStock=" + promisedStock);
+                Handler<AsyncResult<Message<JsonObject>>> handlerResponse = response -> {
+                    if (response.succeeded()) {
+                        container.logger().info("Received reply for an offer");
+                        JsonObject bill = new JsonObject();
+                        bill.putString("action", "bill");
+                        bill.putString("from", id);
+                        bill.putString("charge", response.result().body().getString("from"));
+                        bill.putNumber("quantity", availableQuantity);
+                        bill.putNumber("cost", availableQuantity * cost);
+                        promisedStock -= availableQuantity;
+                        stock -= availableQuantity;
+                        container.logger().info("An offer has been accepted: stock=" + stock + " promisedStock=" + promisedStock);
+                        eventBus.send(adresses.getString("bank"), bill);
+                    } else {
+                        container.logger().info("No reply for an offer");
+                        promisedStock -= availableQuantity;
+                        container.logger().info("stock=" + stock + " promisedStock=" + promisedStock);
+                    }
+                };
+                eventBus.sendWithTimeout(adresses.getString("factory") + message.body().getString("from"), offer, config.getInteger("timeoutReply"), handlerResponse);
             }
         };
         eventBus.registerHandler(adresses.getString("farm"), myHandler);
 
         long timerID = vertx.setPeriodic(config.getInteger("frequency"), new Handler<Long>() {
             public void handle(Long timerID) {
-                stock++;
-                container.logger().info("New stock=" + stock);
+                if (stock < config.getInteger("maxStock")) {
+                    stock++;
+                    container.logger().info("New stock=" + stock);
+                }
             }
         });
 
@@ -57,7 +82,7 @@ public class Farm extends Verticle {
     private void sayHello(EventBus eventBus, JsonObject jsonObject, String id) {
         JsonObject hello = new JsonObject();
         hello.putString("action", "hello");
-        hello.putString("from", "farm "+id);
+        hello.putString("from", id);
         hello.putString("type", "farm");
         hello.putString("version", "1.0");
         eventBus.publish(jsonObject.getString("city"), hello);
