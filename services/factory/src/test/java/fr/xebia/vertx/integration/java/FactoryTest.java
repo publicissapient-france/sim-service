56239fc2b180bf822f0a495ead1610e9915a0967
jerome Doucet <jerdct@gmail.com>
2014-11-23 16:11:03 +0100

    refactor of test class for more clarity

--- services/factory/src/test/java/fr/xebia/vertx/integration/java/FactoryTest.java
+++ services/factory/src/test/java/fr/xebia/vertx/integration/java/FactoryTest.java
@@ -65,50 +65,60 @@ public class FactoryTest extends TestVerticle {
         JsonObject conf = loadConf();
         String id = "TestStore";
         long cancelId = vertx.setPeriodic(15000, l -> {
-            //fail();
+            fail();
         });
+        registerHandlerOnStoreOrderResponse(id, cancelId);
+        // the factory need some resource to answer a store. So a handler on factory 
+        //request for resources is registered and resources sent on request BEFORE
+        //sending some order from a store.
         container.deployVerticle("fr.xebia.vertx.factory.FactoryVerticle", conf, 1, r -> {
             vertx.eventBus().registerHandler("/city/farm", stockRequest -> {
                 if (stockRequest.body() != null && stockRequest.body() instanceof JsonObject) {
                     JsonObject request = (JsonObject) stockRequest.body();
-                    VertxAssert.assertEquals(request.getString("action"), "request");
-                    VertxAssert.assertNotNull(request.getString("from"));
-                    VertxAssert.assertNotNull(request.getNumber("quantity"));
-                    JsonObject stockResponse = new JsonObject().putString("action", "response")
-                            .putString("from", id)
-                            .putNumber("quantity", request.getNumber("quantity"))
-                            .putNumber("cost", 1000);
-
-                    vertx.eventBus().sendWithTimeout("/city/factory/" + request.getString("from"), stockResponse, 5000,
-                            factoryAck -> {
-                                if (factoryAck.failed() 
-                                        || factoryAck.result().body() == null 
-                                        || !(factoryAck.result().body() instanceof JsonObject)) {
-                                    fail();
-                                }
-                            });
-
-                    // then
-                    vertx.eventBus().registerHandler("/city/store/" + id, response -> {
-                        vertx.cancelTimer(cancelId);
-                        if (response.body() != null && response.body() instanceof JsonObject) {
-                            JsonObject hello = (JsonObject) response.body();
-                            VertxAssert.assertEquals(hello.getString("action"), "response");
-                            VertxAssert.assertNotNull(hello.getString("from"));
-                            VertxAssert.assertEquals(hello.getNumber("quantity"), 10);
-                            VertxAssert.assertEquals(hello.getNumber("cost"), 100);
-                            testComplete();
-                        } else {
-                            fail();
-                        }
-
-                    });
+                    sendResourcesToFactory(id, request);
                     JsonObject order = new JsonObject().putString("action", "request").putString("from", id).putNumber(
                             "quantity", 10).putNumber("cost", 100);
                     // when
                     vertx.eventBus().publish("/city/factory", order);
                 }
             });
+        });
+    }
+
+    private void sendResourcesToFactory(String id, JsonObject request) {
+        VertxAssert.assertEquals(request.getString("action"), "request");
+        VertxAssert.assertNotNull(request.getString("from"));
+        VertxAssert.assertNotNull(request.getNumber("quantity"));
+        JsonObject stockResponse = new JsonObject().putString("action", "response")
+                .putString("from", id)
+                .putNumber("quantity", request.getNumber("quantity"))
+                .putNumber("cost", 1000);
+
+        vertx.eventBus().sendWithTimeout("/city/factory/" + request.getString("from"), stockResponse, 5000,
+                factoryAck -> {
+                    if (factoryAck.failed()
+                    || factoryAck.result().body() == null
+                    || !(factoryAck.result().body() instanceof JsonObject)) {
+                        fail();
+                    }
+                });
+    }
+
+    private void registerHandlerOnStoreOrderResponse(String id, long cancelId) {
+        vertx.eventBus().registerHandler("/city/store/" + id, response -> {
+            vertx.cancelTimer(cancelId);
+            if (response.body() != null && response.body() instanceof JsonObject) {
+                // then
+                JsonObject orderResponse = (JsonObject) response.body();
+                VertxAssert.assertEquals(orderResponse.getString("action"), "response");
+                VertxAssert.assertNotNull(orderResponse.getString("from"));
+                VertxAssert.assertEquals(orderResponse.getNumber("quantity"), 10);
+                VertxAssert.assertEquals(orderResponse.getNumber("cost"), 100);
+                testComplete();
+            } else {
+                fail();
+            }
+
         });
     }
 }
