afec817f0dcf5ced449c0986db393e3d0da69e5d
Sergio Dos Santos <sdossantos@xebia.fr>
2014-11-28 00:18:04 +0100

    maj script de déploiement : corrections + renommage des config pour être démarrées avec le script

--- services/services.sh
+++ services/services.sh
@@ -1,51 +1,69 @@
 #!/bin/sh
 
-username="todo"
-hostname="todo"
+# this variable must be configured to point to your remote server, e.g. :
+# remote_server="user@myserver.com"
+# remote_server="user@8.8.8.8"
+# remote_server="alias" if 'alias' is defined in your ~/.ssh/config file
+remote_server="sim-server-alias"
 
-# DO NOT configure variables after this comment
+# DO NOT configure this script after this comment
 script="${0}"
-action="${1?'action as argument #1 must be provided'}"
-module="${2?'module as argument #2 must be provided'}"
+action="${1}"
+module="${2}"
 config="${3:-${module}}" # defaults to module's name
+dirname="$(dirname ${script})"
 
-dirname="$(dirname ${script})";
-runfile="${dirname}/${module}/$(cat ${dirname}/${module}/${module}.main)"
-cfgfile="${dirname}/${module}/${config}.json"
-pidfile="${dirname}/${module}/.${config}.pid"
-logfile="${dirname}/${module}/.${config}.log"
+usage() {
+    echo "${script} '[local-]start|stop|status|restart|list|logs' module [config]" && exit 1
+}
 
 running() {
-    test -f "${pidfile}" && kill -0 $(cat "${pidfile}")
+    test -f "${pidfile}" && kill -0 $(cat "${pidfile}") > /dev/null 2>&1
 }
 
 start() {
-    vertx run "${runfile}" -conf "${cfgfile}" -cluster -cluster-host 0.0.0.0 &> "${logfile}" &
-    echo "${!}" > "${pidfile}"
+    nohup vertx run "${runfile}" -conf "${cfgfile}" -cluster -cluster-host 0.0.0.0 > "${logfile}" 2>&1 & echo "${!}" > "${pidfile}"
 }
 
 stop() {
     kill "$(cat "${pidfile}")" && rm "${pidfile}"
 }
 
-logs() {
-    tail -10f "${logfile}"
-}
+if [ $# -lt 2 ]; then usage; fi
 
 case "${action}" in
-    remote-* )
-        rsync -az "${dirname}" "${username}@${hostname}:services" && ssh "${username}@${hostname}" "services/services.sh ${action#remote-} ${module} ${config}"
-        exit "${?}" ;; # if action is remotely-executed local part of the script must stop here
-    start )
-        running && echo "${config} is still running" || start ;;
-    stop )
-        running && stop || echo "${config} is not running" ;;
-    restart )
-        running && (stop && start) || start ;;
-    status )
-        running && echo "${config} is started" || echo "${config} is stopped" ;;
-    logs )
-        logs ;;
+
+    start | stop | status | restart | list | logs )
+
+        # synchronize files from script directory
+        rsync -az "${dirname}" "${remote_server}:/opt" && ssh "${remote_server}" "bash -l -c \"/opt/services.sh local-${action} ${module} ${config}\"" ;;
+
+    local-start | local-stop | local-status | local-restart | local-list | local-logs )
+
+        # go to module directory, due to vert.x modules' path concerns
+        cd "${dirname}/${module}"
+        runfile="$(cat ${module}.main)"
+        cfgfile="${config}.json"
+        pidfile=".${config}.pid"
+        logfile=".${config}.log"
+        trace="$(whoami)@$(hostname)"
+
+        case "${action#local-}" in
+            start )
+                running && echo "${trace}: ${config} is still running" || start ;;
+            stop )
+                running && stop || echo "${trace}: ${config} is not running" ;;
+            status )
+                running && echo "${trace}: ${config} is started" || echo "${trace}: ${config} is stopped" ;;
+            restart )
+                running && (stop && start) || start ;;
+            list )
+                ls -1a .*.pid | sed "s/\.pid//" | sed "s/\.//" || echo "No ${module} instance running" ;;
+            logs )
+                echo "${trace}: ${logfile}" && tail -10f "${logfile}" ;;
+        esac ;;
+
     * )
-        echo "${script} '[remote-]start|status|logs|stop|restart' module [config]" && exit 1 ;;
+        usage ;;
+
 esac
