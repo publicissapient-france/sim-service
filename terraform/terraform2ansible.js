5bee6b1e32d539388b4af69bc43fd3849236cb7f
Thomas Auffredou <tauffredou@xebia.fr>
2015-01-09 16:48:42 +0100

    Update ansible

--- terraform/terraform2ansible.js
+++ terraform/terraform2ansible.js
@@ -3,48 +3,44 @@
 var fs = require('fs');
 var config = JSON.parse(fs.readFileSync(__dirname + '/terraform.tfstate', 'utf8'));
 var resources = config.modules[0].resources;
-var ansible = {
-    cores: [],
-    factories: []
-};
+var ansible = {};
+var attributes = ['public_ip', 'private_ip','type'];
+
 for (var key in resources) {
     var resource = resources[key];
     if (resource.type == 'aws_instance') {
-        var host = {
-            public_ip: resource.primary.attributes.public_ip,
-            private_ip: resource.primary.attributes.private_ip
-        };
-        if (key.indexOf('aws_instance.core') != -1) {
-            host.type = 'core';
-            ansible.cores.push(host);
-        }
-        if (key.indexOf('aws_instance.factory') != -1) {
-            host.type = 'factory';
-            ansible.factories.push(host)
+        var host = resource.primary.attributes;
+        var myRegexp = /aws_instance\.([^.]*)/g;
+        var match = myRegexp.exec(key);
+        var type = match[1];
+        if (match) {
+            host.type = type;
+            if (ansible[type] == null) {
+                ansible[type] = [];
+            }
+            ansible[type].push(host);
         }
     }
 }
 
-var stream = fs.createWriteStream("private/hosts");
+var stream = fs.createWriteStream("private/inventory");
 stream.once('open', function (fd) {
     function writeHost(host) {
-
         stream.write(host.public_ip);
         for (var key in host) {
-            stream.write(" " + key + "=" + host[key]);
+            if (attributes.indexOf(key) != -1) {
+                stream.write(" " + key + "=\"" + host[key] + "\"");
+            }
         }
         stream.write("\n");
     }
 
-    stream.write("[cores]\n");
-    ansible.cores.forEach(function (host) {
-        writeHost(host);
-    });
-    stream.write("\n");
+    for (var key in ansible) {
+        stream.write("[" + key + "]\n");
+        ansible[key].forEach(function (host) {
+            writeHost(host);
+        });
+    }
 
-    stream.write("[factories]\n");
-    ansible.factories.forEach(function (host) {
-        writeHost(host);
-    });
     stream.end();
 });
